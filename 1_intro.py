e7a3b4bbb9c089bd521dc1bbaf2fb59a632e6a75
shatahaq <shatadiyaulhaq@gmail.com>
2024-09-03 12:33:18 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,17 @@
+# Deklarasi Variabel
+a = 10
+b = 5
+c = a + b 
+perkalian = a*b
+pembagian = a/b
+pengurangan = a-b
+modulo = a%b
+pangkat = a**b
+
+# Operasi (Operation) : tmabah (+), kurang (-), Bagi (/), kali(*), Modulo (%), pangkat (**)
+print(c)
+print(pengurangan)
+print(pembagian)
+print(perkalian)
+print(modulo)
+print(pangkat)
